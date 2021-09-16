028229ca1322bfe36ce615cb9d1efda7dd3bdb0b
ika-gyosen <naruse42@gmail.com>
2021-09-17 00:21:55 +0900

    うごいた！！！

--- src/containers/pages/index.tsx
+++ src/containers/pages/index.tsx
@@ -1,43 +1,25 @@
 import React, { useEffect, useState } from "react";
 import Peer from "skyway-js";
 
-// const peer = new Peer({ key: "7f6811d4-2b08-4bd7-8be8-cd036923e473" });
+const peer = new Peer({ key: "7f6811d4-2b08-4bd7-8be8-cd036923e473" });
 
 function ChatApp() {
   const [peerId, setPeerId] = useState("");
   const [isOpen, setIsOpen] = useState(false);
   const [userName, setuserName] = useState("");
   const [roomName, setroomName] = useState("new room");
-  const [roomName2, setroomName2] = useState("new room");
+  // const [roomName2, setroomName2] = useState("new room");
+  //   const [roomObj, setroomObj] = useState("new room");
+
   const [message, setMessage] = useState<string[]>([]);
   // console.log(peer);
   useEffect(() => {
-    const peer = new Peer({ key: "7f6811d4-2b08-4bd7-8be8-cd036923e473" });
     peer.on("open", () => {
       console.log("id", peer.id);
       setPeerId(peer.id);
       setIsOpen(peer.open);
-
-      if (roomName2.length > 0) {
-        const room = peer.joinRoom(roomName2, { mode: "mesh" });
-        room.on(
-          "data",
-          ({
-            data,
-            src,
-          }: {
-            data: { user: string; text: string };
-            src: string;
-          }) => {
-            console.log("test", data, message);
-            const newArray = [...message, data.text];
-            setMessage(newArray);
-            console.log("zzzzzzzzzz", message);
-          }
-        );
-      }
     });
-  }, [message, roomName2]);
+  }, []);
 
   const userNameHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setuserName(e.target.value);
@@ -48,25 +30,25 @@ function ChatApp() {
   };
 
   const joinHandler = () => {
-    // const roomHandler = (room: any) => {
-    //   room.on(
-    //     "data",
-    //     ({
-    //       data,
-    //       src,
-    //     }: {
-    //       data: { user: string; text: string };
-    //       src: string;
-    //     }) => {
-    //       console.log("test", data, message);
-    //       setMessage([...message, data.text]);
-    //       console.log("zzzzzzzzzz", message);
-    //     }
-    //   );
-    // };
-    // const roomItem = peer.joinRoom(roomName, { mode: "mesh" });
-    // roomHandler(roomItem);
-    setroomName2(roomName);
+    const roomHandler = (room: any) => {
+      room.on(
+        "data",
+        ({
+          data,
+          src,
+        }: {
+          data: { user: string; text: string };
+          src: string;
+        }) => {
+          console.log("test", data, message);
+          setMessage((prev) => [...prev, data.text]);
+          console.log("zzzzzzzzzz", message);
+        }
+      );
+    };
+    const roomItem = peer.joinRoom(roomName, { mode: "mesh" });
+    roomHandler(roomItem);
+    // setroomName2(roomName);
   };
 
   return (
